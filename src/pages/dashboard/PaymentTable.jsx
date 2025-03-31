607805332f0f218ae125b15ab3bb05509501317d
AayushJain27 <ayush@serviceplug.in>
2025-03-31 16:55:19 +0530

    fixed url

--- src/pages/dashboard/PaymentTable.jsx
+++ src/pages/dashboard/PaymentTable.jsx
@@ -164,7 +164,7 @@ export default function PaymentTable(props) {
 
   const handleCopyLink = (id) => {
     console.log(id, 'dmrkfmk');
-    const link = `https://omify-ayush-jains-projects-4b8070bf.vercel.app//omify/contentPage?id=${encodeURIComponent(id)}`;
+    const link = `https://omify-ayush-jains-projects-4b8070bf.vercel.app/omify/contentPage?id=${encodeURIComponent(id)}`;
     console.log(link, 'dmrkmf');
     navigator.clipboard
       .writeText(link)
